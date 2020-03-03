a362dc7c7dc8cc6edff018aae71aa231c2d74340
Connor Murphy <murphyc97@gmail.com>
2020-03-02 23:07:16 -0500

    init the changes

--- Controllers/ToDoItemsController.cs
+++ Controllers/ToDoItemsController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Todo.Api.Models;
+using Todo.Api.Services;
 
 namespace Todo.Api.Controllers
 {
@@ -8,13 +9,19 @@ namespace Todo.Api.Controllers
     [Route("api/todos")]
     public class TodoItemsController : ControllerBase
     {
+        private TodoRepository repository;
+        public TodoItemsController()
+        {
+            repository = new TodoRepository();
+        }
         [HttpGet]
         public IActionResult GetTodos()
         {
-            var result = new List<TodoItem>(){
-                new TodoItem{ Id = 1, Action="Thing one.", IsDone=true},
-                new TodoItem{ Id = 2, Action="Thing two.", IsDone=false},
-            };
+            var result = repository.GetAllTodoItems();
+            if (result == null)
+            {
+                return NotFound();
+            }
 
             return Ok(result);
         }
